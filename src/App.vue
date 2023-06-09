2dab2981f90c7957b8f077ed1975bcd13627d0b5
Rostislav <rostik057@gmail.com>
2023-06-09 21:51:04 +0300

    !fix fonts

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,40 @@ import TheFooter from '@/components/TheFooter/TheFooter.vue';
 <style lang="scss">
 @import '@/app/styles/index.scss';
 
+@font-face {
+  font-family: 'PT Sans';
+  src: url('@/app/fonts/PTSansBold.woff2') format('woff2'),
+    url('@/app/fonts/PTSansBold.woff') format('woff');
+  font-weight: bold;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'PT Sans';
+  src: url('@/app/fonts/PTSansRegular.woff2') format('woff2'),
+    url('@/app/fonts/PTSansRegular.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'PT Sans Regular Cyr';
+  src: url('@/app/fonts/PTSansRegularCyr.woff2') format('woff2'),
+    url('@/app/fonts/PTSansRegularCyr.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'PT Sans Bold Cyr';
+  src: url('@/app/fonts/PTSansBoldCyr.woff2') format('woff2'),
+    url('@/app/fonts/PTSansBoldCyr.woff') format('woff');
+  font-weight: bold;
+  font-style: normal;
+}
+
 .wrapper {
+  font-family: 'PT Sans';
   display: flex;
   flex-direction: column;
   min-height: 100vh;
