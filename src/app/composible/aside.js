1621e4162bfffb1bf87e22e211594b9cf87d8850
Rostislav <rostik057@gmail.com>
2023-06-14 20:01:16 +0300

    !fix header burger mobile

--- src/app/composible/aside.js
+++ src/app/composible/aside.js
@@ -2,6 +2,10 @@ import { ref } from 'vue';
 
 export const isActiveAside = ref(false);
 
+export const setIsActiveAsideToggle = () => {
+    isActiveAside.value = !isActiveAside.value;
+};
+
 export const setIsActiveAsideTrue = () => {
     isActiveAside.value = true;
 };
