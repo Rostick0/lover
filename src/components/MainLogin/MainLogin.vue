a03ac4844d23b2d30d49468a95b1f67649a1b0fe
Rostislav <rostik057@gmail.com>
2023-06-14 20:38:13 +0300

    !fix mobile main page

---
+++ src/components/MainLogin/MainLogin.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="main-login">
+        <div class="main-login__mobile_content">
+            <div class="main-login__logo">
+                <img width="240" src="@/app/assets/image/logo.png" alt="Лого">
+            </div>
+            <div class="main-login__title">В этот раз я найду свою любовь!</div>
+        </div>
+        <RouterLink to="/profile/1" class="">
+            <UiButton class="main-login__button">
+                <svg data-v-12e41b4d="" width="24" height="16" viewBox="0 0 27 18" xmlns="http://www.w3.org/2000/svg"
+                    class="auth-btn__icon icon-right">
+                    <path data-v-12e41b4d=""
+                        d="M2.75.76h16.5c1.008 0 1.833.824 1.833 1.833V6.25H19.25V2.574H2.75v12.861h16.5V11.75h1.833v3.676a1.823 1.823 0 0 1-1.833 1.815H2.75a1.823 1.823 0 0 1-1.833-1.815V2.593c0-1.018.825-1.834 1.833-1.834Zm20.667 11.907L27.083 9l-3.666-3.667v2.75H14.25v1.834h9.167v2.75Z"
+                        fill="#FFF" fill-rule="evenodd"></path>
+                </svg>
+                <span>Войти</span>
+            </UiButton>
+        </RouterLink>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import './login.scss';
+</style>
--- src/pages/MainPage/MainPage.vue
+++ src/pages/MainPage/MainPage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import MainForm from '@/components/MainForm/MainForm.vue';
 import MainSlider from '@/components/MainSlider/MainSlider.vue';
+import MainLogin from '@/components/MainLogin/MainLogin.vue';
 
 const peoples = [
     {
@@ -52,23 +53,15 @@ const peoples = [
 <template>
     <div class="main">
         <div class="main__top">
-            <div class="container-main">
+            <div class="container-main main__top_container">
                 <div class="main__registration registration">
                     <div class="registration__content">
                         <div class="registration__content_left">
                             <MainForm></MainForm>
                         </div>
-                        <RouterLink to="/profile/1" class="registration__content_right">
-                            <UiButton class="registration__log">
-                                <svg data-v-12e41b4d="" width="24" height="16" viewBox="0 0 27 18"
-                                    xmlns="http://www.w3.org/2000/svg" class="auth-btn__icon icon-right">
-                                    <path data-v-12e41b4d=""
-                                        d="M2.75.76h16.5c1.008 0 1.833.824 1.833 1.833V6.25H19.25V2.574H2.75v12.861h16.5V11.75h1.833v3.676a1.823 1.823 0 0 1-1.833 1.815H2.75a1.823 1.823 0 0 1-1.833-1.815V2.593c0-1.018.825-1.834 1.833-1.834Zm20.667 11.907L27.083 9l-3.666-3.667v2.75H14.25v1.834h9.167v2.75Z"
-                                        fill="#FFF" fill-rule="evenodd"></path>
-                                </svg>
-                                <span>Войти</span>
-                            </UiButton>
-                        </RouterLink>
+                        <div class="registration__content_right">
+                            <MainLogin></MainLogin>
+                        </div>
                     </div>
                 </div>
             </div>
